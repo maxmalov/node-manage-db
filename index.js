4c9a35830a41dbe541e77e392934a35deb4cc9fd
Maxim Malov <m.malov.v@gmail.com>
2014-10-31 22:59:06 +0600

    Refactor database adapter loading

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 'use strict';
 
+var join = require('path').join;
+
 module.exports = function (opts) {
   var dialect = opts && opts.dialect || 'postgres';
   var provider;
 
-  switch(dialect) {
-    case 'postgres':
-      provider = require('./lib/postgres');
-      break;
-    case 'mysql':
-      provider = require('./lib/mysql');
-      break;
-    default:
-      throw new Error('Dialect: ' + dialect + ' is not supported for now. Sorry ;(');
+  try {
+    provider = require(join('./lib', dialect));
+  } catch (e) {
+    throw new Error('Dialect: ' + dialect + ' is not supported for now. Sorry ;(');
   }
 
   return provider(opts);
