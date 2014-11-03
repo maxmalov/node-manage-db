00058d81b80d742279b74c726a8e700e24bb848f
Maxim Malov <m.malov.v@gmail.com>
2014-11-03 12:17:51 +0600

    Fix unsupported dialect error

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 var join = require('path').join;
 
+var DIALECTS = ['postgres', 'mysql'];
+
 module.exports = function (opts) {
   var dialect = opts && opts.dialect || 'mysql';
   var provider;
 
-  try {
-    provider = require(join('./lib', dialect));
-  } catch (e) {
+  if (DIALECTS.indexOf(dialect) === -1) {
     throw new Error('Dialect: ' + dialect + ' is not supported for now. Sorry ;(');
   }
 
+  provider = require('./' + join('lib', dialect));
   return provider(opts);
 };
